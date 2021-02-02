83888ab8618289842160c287c3069a4f659f56f6
Josh Roach <josh.roach@D2L.com>
2021-02-02 13:42:07 -0500

    remove none lang term

--- lang/pt.js
+++ lang/pt.js
@@ -4,7 +4,6 @@ export default {
 	"editTagline": "Click to edit tagline",
 	"email": "Email",
 	"instantMessage": "Instant Message",
-	"none": "None",
 	"offline": "Offline",
 	"online": "Online",
 	"userProgress": "User Progress"
