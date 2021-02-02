3fe57176f136b6ac82c6d298c8e33a05f000ed97
Josh Roach <josh.roach@D2L.com>
2021-02-02 13:18:47 -0500

    Add serge support

---
+++ lang/es-es.js
@@ -0,0 +1,11 @@
+/* eslint quotes: 0 */
+
+export default {
+	"editTagline": "Click to edit tagline",
+	"email": "Email",
+	"instantMessage": "Instant Message",
+	"none": "None",
+	"offline": "Offline",
+	"online": "Online",
+	"userProgress": "User Progress"
+};
