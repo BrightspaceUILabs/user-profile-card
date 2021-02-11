178d54ee27b173f9fbfcb127d110088660c62285
Scy Akulick <50635849+Scy-D2L@users.noreply.github.com>
2021-02-11 17:04:01 -0500

    Add display name tabstop (#32)
    * Add display name tabstop

--- user-profile-card.js
+++ user-profile-card.js
@@ -271,11 +271,12 @@ class UserProfileCard extends LocalizeUserProfileCard(LitElement) {
 			<div class="${classMap(classes)}">
 				<slot name="illustration" class="d2l-link" title="${this.localize('openProfile', {displayName : this.displayName})}"  @click="${this._onProfileImageClick}"></slot>
 				<div class="d2l-labs-profile-card-basic-info">
-					<a class="d2l-heading-2 d2l-labs-profile-card-name d2l-link" title="${this.localize('openProfile', {displayName : this.displayName})}" @click="${this._onDisplayNameClick}">${this.displayName}</a>
+					<a class="d2l-heading-2 d2l-labs-profile-card-name d2l-link" tabindex="0" title="${this.localize('openProfile', {displayName : this.displayName})}" @click="${this._onDisplayNameClick}">${this.displayName}</a>
 					${this._renderOnlineStatus()}
-					<ul class="d2l-labs-profile-card-attributes d2l-body-small">
-						${this.userAttributes.map((item) => html`<li>${item}</li>`)}
-					</ul>
+					${this.userAttributes.length > 1 ? html`
+						<ul class="d2l-labs-profile-card-attributes d2l-body-small">
+							${this.userAttributes.map((item) => html`<li>${item}</li>`)}
+						</ul>` : html`` }
 				</div>
 				${this._renderProfileCardContent()}
 				${this._renderAwardIcons()}
