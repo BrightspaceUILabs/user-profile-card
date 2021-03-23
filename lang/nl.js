b938257b2c7211e3c4d79aed18129f891cd453b3
JenkinsReader <resupport@desire2learn.com>
2021-03-23 12:12:03 -0400

    Translation Updates

--- lang/nl.js
+++ lang/nl.js
@@ -1,11 +1,11 @@
 /* eslint quotes: 0 */
 
 export default {
-	"editTagline": "Klik om de kopregel te bewerken",
-	"email": "E-mail",
-	"instantMessage": "Chatbericht",
-	"offline": "Offline",
-	"online": "Online",
-	"openProfile": "Open het profiel van {displayName} in een nieuw venster",
-	"userProgress": "Voortgang gebruiker"
+	"editTagline": "$$$$$ $$$ $$$ $$$$$$$$$$ $$$ $$$$$$$$$$",
+	"email": "$-$$$$$",
+	"instantMessage": "$$$$$$$$$$$$$$",
+	"offline": "$$$$$$$$$",
+	"online": "$$$$$$$$",
+	"openProfile": "$$$$$ $$$$ $$$$$$$$$ $$$$ {displayName} $$$ $$$$ $$$$$$$ $$$$$$$$$",
+	"userProgress": "$$$$$$$$$$$$ $$$$$$$$$$$$"
 };
